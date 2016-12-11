0d94559addf1c7f667a1448cfe2d7ac498ac862d
Ahmad N. F <fahmadnaufal@gmail.com>
2016-12-12 02:19:38 +0700

    init project

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import jess.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            Rete r = new Rete();
+            r.eval("(deffunction square(?n) (return (* ?n ?n)))");
+            Value v = r.eval("(square 3)");
+
+            System.out.println(v.intValue(r.getGlobalContext()));
+        } catch (JessException e) {
+            e.printStackTrace();
+        }
+    }
+}
